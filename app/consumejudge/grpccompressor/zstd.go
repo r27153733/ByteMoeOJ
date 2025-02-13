5d7dd8e4b4391c2e08f33c89becc777b315761c6
r27153733 <r27153733@gmail.com>
2025-02-13 19:35:32 +0800

    feat: grpc zstd pool.

--- app/consumejudge/grpccompressor/zstd.go
+++ app/consumejudge/grpccompressor/zstd.go
@@ -44,14 +44,58 @@ func releaseZstdWriter(zw *zstd.Encoder) { //nolint:unused
 	zstdEncoderPool.Put(zw)
 }
 
+type ZstdReleaseWriter struct {
+	*zstd.Encoder
+}
+
+func (w ZstdReleaseWriter) Close() error {
+	err := w.Encoder.Close()
+	releaseZstdWriter(w.Encoder)
+	return err
+}
+
+func acquireZstdReleaseWriter(w io.Writer) (io.WriteCloser, error) {
+	writer, err := acquireZstdWriter(w)
+	if err != nil {
+		return writer, err
+	}
+	return ZstdReleaseWriter{Encoder: writer}, nil
+}
+
+type ZstdReleaseReader struct {
+	*zstd.Decoder
+	isEnd bool
+}
+
+func acquireZstdReleaseReader(r io.Reader) (io.Reader, error) {
+	reader, err := acquireZstdReader(r)
+	if err != nil {
+		return nil, err
+	}
+	return &ZstdReleaseReader{Decoder: reader}, nil
+}
+
+func (r *ZstdReleaseReader) Read(p []byte) (int, error) {
+	if r.isEnd {
+		return 0, io.EOF
+	}
+	read, err := r.Decoder.Read(p)
+	if err == io.EOF {
+		r.isEnd = true
+		releaseZstdReader(r.Decoder)
+	}
+
+	return read, err
+}
+
 type ZstdCompressor struct{}
 
 func (z ZstdCompressor) Compress(w io.Writer) (io.WriteCloser, error) {
-	return acquireZstdWriter(w)
+	return acquireZstdReleaseWriter(w)
 }
 
 func (z ZstdCompressor) Decompress(r io.Reader) (io.Reader, error) {
-	return acquireZstdReader(r)
+	return acquireZstdReleaseReader(r)
 }
 
 func (z ZstdCompressor) Name() string {
